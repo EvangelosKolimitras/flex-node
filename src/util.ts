480146261d101038a2bf5556515d02120db3ff2b
= <evangeloskolimitras@outlook.com>
2022-12-25 21:30:52 +0100

    update utils

--- src/util.ts
+++ src/util.ts
@@ -3,17 +3,28 @@ export const registry: { [key: HTMLElement['id']]: Node } = {};
 export const NewNode = (options: { w?: [number, 'px' | '%']; h?: [number, 'px' | '%']; bg?: string; c?: string; txt?: string }) => {
 	const { w, h, bg, c, txt } = options;
 	const div = document.createElement('div');
-	const uuid = () => Math.random() * 10e10;
+	const uuid = () =>
+		[
+			crypto.getRandomValues(new Uint32Array(1))[0].toString(36),
+			crypto.getRandomValues(new Uint32Array(1))[0].toString(36),
+			crypto.getRandomValues(new Uint32Array(1))[0].toString(36),
+			crypto.getRandomValues(new Uint32Array(1))[0].toString(36),
+		].join('-');
+
 	div.id = `${uuid()}`;
 	div.style.width = `${w?.[0]}${w?.[1]}`;
 	div.style.height = `${h?.[0]}${h?.[1]}`;
-	div.style.backgroundColor = bg ? bg : 'black';
-	div.style.color = c ? c : 'white';
-	div.style.boxShadow = '0 0 5px 0 ' + (c ? c : bg);
+	div.style.backgroundColor = bg || 'black';
+	div.style.color = c || 'white';
+	div.style.boxShadow = '0 0 5px 0 ' + (c || bg);
 	div.style.borderRadius = '5px';
 	div.style.fontSize = '2rem';
-	if (txt) div.textContent = txt;
-	if (registry[div.id]) registry[div.id] = div;
+	if (txt) {
+		div.textContent = txt;
+	}
+	if (registry[div.id]) {
+		registry[div.id] = div;
+	}
 	return div;
 };
 
