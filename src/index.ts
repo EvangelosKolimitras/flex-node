520d1e5e55ef88a5acf13a0d46369f2a1ad50785
= <evangeloskolimitras@outlook.com>
2023-01-02 22:19:04 +0100

    update example

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,33 @@ export const FlexContainer = (options: { node: Node }): FlexContainerReturnType 
 export const FlexItem = (options: ItemOptions): FlexItemReturnType => {
 	return Item(options);
 };
+
+/**
+ * Example
+ */
+
+// const div1 = document.createElement('div');
+// div1.style.width = '200px';
+// div1.style.height = '200px';
+// div1.style.backgroundColor = 'red';
+// document.body.appendChild(div1);
+
+// const div2 = document.createElement('div');
+// div2.style.width = '200px';
+// div2.style.height = '200px';
+// div2.style.backgroundColor = 'green';
+// document.body.appendChild(div2);
+
+// const div3 = document.createElement('div');
+// div3.style.width = '200px';
+// div3.style.height = '200px';
+// div3.style.backgroundColor = 'blue';
+// document.body.appendChild(div3);
+
+// document.body.style.height = '100vh';
+// const flexedWrapperDiv = FlexContainer({ node: document.body });
+// flexedWrapperDiv.AlignItems('center').JustifyContent('center');
+// FlexItem({
+// 	container: flexedWrapperDiv.node,
+// 	at: [1, 2],
+// }).AlignSelf('flex-end');
